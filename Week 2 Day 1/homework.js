914419de4d41c28b215a12541ec8a77c272500b6
Aaron Combs <streeteducated512@gmail.com>
2016-12-18 22:31:42 -0600

    Add files via upload

---
+++ Week 2 Day 1/homework.js
@@ -0,0 +1,65 @@
+ function add(a,b){
+    var answer = a + b;
+
+    console.log(answer);
+    return answer;
+}
+
+function subtract(a,b){
+    var answer = a - b;
+    
+    console.log(answer);
+    return answer;
+}
+
+function multiply(a,b){
+   var answer = a * b;
+    
+    console.log(answer);
+    return answer;
+}
+
+function divide(a,b){
+    var answer = a / b;
+    
+    console.log(answer);
+    return answer;
+}
+
+
+
+var a = add(5,6);
+var b = subtract(12,2);
+var c = add(a,b);
+
+var x = multiply(5,6);
+var y = divide(80,12);
+var z = multiply(x,y);
+
+console.log(`The variable c is equal to ${c} and the variable z is equal to ${z}`);
+
+var pet = ('dempsey');
+var toes = 10;
+var tired = true;
+var fruits = ('apples', 'oranges', 'grapes');
+    fruits.reverse();
+
+var friends = [
+    ['bob', 'red'], ['jim', 'blue'], ['tom', 'yellow']
+];
+
+var person = {name: 'tom', toeCount: 10, hairColor: 'brown'};
+   console.log(name, toeCount, hairColor);
+
+function squared(){
+    var x = Math.sqrt(10);
+    return squared;
+}
+
+function area(){
+    var length = 10;
+    var width = 5;
+    console.log(length*width);
+}
+
+Math.random();
